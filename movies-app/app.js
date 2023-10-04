45b13858e4253bc2488bc5e275e424da22e93164
Jomkit <j.jujaroen96@gmail.com>
2023-10-04 23:40:27 +0700

    Stuck on the last problem, sorting the movies

--- movies-app/app.js
+++ movies-app/app.js
@@ -2,6 +2,16 @@ $('#form-submit').on('click', function(e) {
     e.preventDefault();
     let title = $('input[name="Movie-Title"]').val();
     let rating = $('input[name="Rating"]').val();
+
+    if(rating < 1 || rating > 10){
+        alert("Rating out of range");
+        return false;
+    }
+    if(title.length < 2){
+        alert("Movie title is too short");
+        return false;
+    }
+    
     $(`<li class="movie">${title}, ${rating}/10 <span class="remove-btn">&#9746;</span></li>`).appendTo($('#movies'));
     $('.form-input').val('');
 })
