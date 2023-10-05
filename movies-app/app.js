4a665ac807bcb2055c45a9c42b546fc3e0575c7d
Jomkit <j.jujaroen96@gmail.com>
2023-10-05 10:43:15 +0700

    Finished further study

--- movies-app/app.js
+++ movies-app/app.js
@@ -1,3 +1,4 @@
+/* Handle form submission with input validation */
 $('#form-submit').on('click', function(e) {
     e.preventDefault();
     let title = $('input[name="Movie-Title"]').val();
@@ -11,11 +12,57 @@ $('#form-submit').on('click', function(e) {
         alert("Movie title is too short");
         return false;
     }
-    
-    $(`<li class="movie">${title}, ${rating}/10 <span class="remove-btn">&#9746;</span></li>`).appendTo($('#movies'));
+    // Adds rated movies to a list
+    $(`<li class="movie">${title}, <span class="movie-score">${rating}</span>/10 <span class="remove-btn">&#9746;</span></li>`).appendTo($('#movies'));
     $('.form-input').val('');
 })
 
+/* remove function for each move list item */
 $('ul').on('click', '.movie', function(e) {
     $(this).remove();
 })
+
+/** sort alphabetical function */
+function sortAlph(selector){
+    $(selector).children('li').sort((a,b) => {
+        let A = $(a).text().toUpperCase();
+        let B = $(b).text().toUpperCase();
+        return (A < B) ? -1 : (A > B) ? 1 : 0;
+    }).appendTo(selector);
+}
+
+/** Sort alphabetical button */
+$('#alphabetical').on('click', function() {
+    console.log("sort clicked");
+    sortAlph('ul#movies');
+})
+
+/** Sort ascending function */
+function sortAscend(selector){
+    $(selector).children('li').sort((a,b) => {
+        let A = $(a.querySelector('.movie-score')).text();
+        let B = $(b.querySelector('.movie-score')).text();
+        console.log(A);
+        return (A - B);
+    }).appendTo(selector);
+}
+
+/** sort ascending button */
+$('#ascend').on('click', function() {
+    sortAscend('ul#movies');
+});
+
+/** Sort descending function */
+function sortDescend(selector){
+    $(selector).children('li').sort((a,b) => {
+        let A = $(a.querySelector('.movie-score')).text();
+        let B = $(b.querySelector('.movie-score')).text();
+        console.log(A);
+        return (B - A);
+    }).appendTo(selector);
+}
+
+/** sort descending button */
+$('#descend').on('click', function() {
+    sortDescend('ul#movies');
+});
